722fea318943aa09487bc9c1aaccb7c7c2b23279
shohibb <akmalshahibmaulana@gmail.com>
2023-12-14 15:06:55 +0700

    latihan modul 6

--- Modul6/Percobaan1.py
+++ Modul6/Percobaan1.py
@@ -1,7 +1,7 @@
 from PIL import Image, ImageDraw, ImageFont
 
 # TODO 1: Lakukan load image pada variabel berikut
-gambarku = Image.open(r"Modul6\UMM.jpg")
+gambarku = Image.open(r"Modul6\UMM.png")
 
 # TODO 2: Ubah gambar menjadi hitam-putih
 gambarBW = gambarku.convert("L")
@@ -13,12 +13,12 @@ draw = ImageDraw.Draw(gambarBW)
 font = ImageFont.truetype(direktoriFont, size)
 text = "Akmal shahib maulana 2021-135"
 text_width = draw.textlength(text, font)
-text_y = gambarku.height - font.size  # Update untuk pemosisian tengah vertikal
-text_x = (gambarku.width - text_width) // 2  # Update untuk pemosisian tengah horizontal
-draw.text((text_x, text_y), text, -1, font)
+text_y = gambarku.height - font.size
+text_x = (gambarku.width - text_width) // 2
+draw.text((text_x, text_y), text, 225, font)
 
 # TODO 4: Simpan gambar hasil edit menggunakan fungsi save()
-gambarBW.save("Modul6\percobaan_satu.jpg")
+gambarBW.save("Modul6\percobaan_satu.png")
 
 # TODO 5: Tampilkan hasil akhir gambar
 gambarBW.show()
---
+++ Modul6/Percobaan2.py
@@ -0,0 +1,32 @@
+# TODO 0 : Import beberapa library lain yang dibutuhkan
+from PIL import Image, ImageDraw, ImageFont
+
+# TODO 1 : Buka gambar latar belakang (background) dan gambar yang ingin disisipkan (overlay)
+background_image = Image.open(r"Modul6\view-umm.jpg")
+overlay_image = Image.open(r"Modul6\UMM.png")
+
+
+# TODO 2 : Konversi overlay image ke mode RGB (menghilangkan alpha channel)
+overlay_image = overlay_image.convert("RGB")
+
+
+# TODO 3 : (optional) Perkecil ukuran gambar overlay menggunakan method resize()
+resize = overlay_image.resize((400, 150))
+
+# Tentukan/Hitung koordinat tengah untuk menempatkan overlay
+background_width, background_height = background_image.size
+overlay_width, overlay_height = resize.size
+
+# Hitung koordinat tengah
+x_center = (background_width - overlay_width) // 2
+y_center = (background_height - overlay_height) // 2
+
+# TODO 4 : Sisipkan gambar overlay ke dalam gambar background menggunakan method paste()
+background_image.paste(resize, (x_center, y_center))
+
+
+# TODO 5 : Simpan gambar hasil edit
+background_image.save(r"Modul6\overlay.jpg")
+
+# TODO 6 : Tampilkan
+background_image.show()
