f9d426479e3bd4f661128332540275ee96f2edcc
shohibb <akmalshahibmaulana@gmail.com>
2023-12-14 15:47:17 +0700

    latihan modul 6

--- Modul6/Percobaan3.py
+++ Modul6/Percobaan3.py
@@ -13,5 +13,9 @@ contrast = curr_cont.enhance(new_count)
 
 
 contrast.save(r"Modul6\brightness_contrast_image.jpg")
-brightness.show()
-# contrast.show()
+
+merged_image = Image.new("RGB", (img.width * 2, img.height))
+merged_image.paste(img, (0, 0))
+merged_image.paste(contrast, (img.width, 0))
+
+merged_image.show()
